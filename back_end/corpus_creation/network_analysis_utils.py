45aff99a9b2e4f6ac6f096b5f3ddaaebc0e3055c
siskavera <sskvera@gmail.com>
2017-01-28 23:14:42 +0000

    added keyword overlap measure

--- back_end/corpus_creation/network_analysis_utils.py
+++ back_end/corpus_creation/network_analysis_utils.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import gzip
-
+import re
+from collections import defaultdict
 
 def calculate_overlap(G, source_paper, target_paper):
     source_edges = set(G[source_paper].keys())
@@ -26,7 +27,46 @@ def add_second(G):
         for node2 in set(G[node1].keys()):
             for node3 in set(G[node2].keys()):
                 if not H.has_edge(node1, node3):
-                    H.add_edge(node1, node3, first=0, second=0)
+                    H.add_edge(node1, node3)
                 if not G.has_edge(node1, node3):
                     H[node1][node3]['second'] += 1
     return H
+
+# Note: compares everything, makes it into a full graph
+def add_keyword_overlap(G):
+    for node1 in G.nodes():
+        for node2 in G.nodes():
+            if node1 != node2:
+                n_overlaps = measure_keyword_overlap(G, node1, node2)
+                if n_overlaps > 0:
+                    if not G.has_edge(node1, node2):
+                        G.add_edge(node1, node2)
+                    if not 'keyword_overlap' in G[node1][node2].keys():
+                        G[node1][node2]['keyword_overlap'] = n_overlaps
+    return G
+
+def measure_keyword_overlap(G, node1, node2):
+    list1 = set(re.split("[; ]", G.node[node1]['key_words']))
+    list2 = set(re.split("[; ]", G.node[node2]['key_words']))
+    overlap_measure = 0.0
+
+    keyword_weights = calculate_keyword_weights(G)
+
+    for word1 in list1:
+        for word2 in list2:
+            if word1 == word2:
+                overlap_measure += keyword_weights[word1]
+    return overlap_measure
+
+def calculate_keyword_weights(G):
+    key_word_weights = defaultdict(float)
+
+    for node in G.nodes():
+        key_words = set(re.split("[; ]", G.node[node]['key_words']))
+        for key_word in key_words:
+            key_word_weights[key_word] += 1
+
+    for key_word in key_word_weights.keys():
+        key_word_weights[key_word] = 1/key_word_weights[key_word]
+        
+    return key_word_weights
