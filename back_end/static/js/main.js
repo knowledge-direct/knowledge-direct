7b8a09e950bfb46d3e9d3a45ec1c660c02c9add7
Jack Hughes <jack@jackhughesweb.com>
2017-01-28 18:37:11 +0000

    Moved templating to Jinja

--- back_end/static/js/main.js
+++ back_end/static/js/main.js
@@ -1,14 +1,7 @@
 (function($) {
 
   $(document).ready(function() {
-    $.getJSON('/logged_in', function (data) {
-      if (data['status']) {
-        $('.user-name').text(data['name']);
-        $('.logged-in').removeClass('hide');
-      } else {
-        $('.logged-out').removeClass('hide');
-      }
-    });
+    
   });
 
 })(jQuery);
