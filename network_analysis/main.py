b5c900119fff9b4d4b935401d16f9955d5c03ea8
siskavera <sskvera@gmail.com>
2017-01-28 21:19:01 +0000

    Got rid of edges to papers without info

--- network_analysis/main.py
+++ network_analysis/main.py
@@ -2,19 +2,37 @@ import sys
 sys.path.append("../back_end/db")
 sys.path.append("../back_end")
 
+from nltk.corpus import biocreative_ppi
+
 import read_graph
 import recommend_papers
-
 import database
 
-#citation_graph = read_graph.read_cit_HepPh()
+citation_graph = read_graph.read_cit_HepPh()
 db = database.Database()
 citation_graph = db.get_citation_network()
 citation_graph = read_graph.add_first(citation_graph)
-#citation_graph = read_graph.add_second(citation_graph)
+citation_graph = read_graph.add_second(citation_graph)
 
-print(len(citation_graph.nodes()))
-print(citation_graph.nodes(data=True))
+#print(citation_graph.nodes(data=True))
+node1 = list(citation_graph.nodes())[1]
+node2 = list(citation_graph.nodes())[2]
+
+list1 = set(citation_graph.node[node1]['key_words'].split(";"))
+list2 = set(citation_graph.node[node2]['key_words'].split(";"))
+similarity=dict()
+
+for word1 in list1:
+    for word2 in list2:
+        wordFromList1 = biocreative_ppi.synsets(word1)
+        wordFromList2 = biocreative_ppi.synsets(word2)
+        if wordFromList1 and wordFromList2:  # Thanks to @alexis' note
+            s = wordFromList1[0].wup_similarity(wordFromList2[0])
+            similarity[word1+"_"+word2]=s
+
+print(list1)
+print(list2)
+print(similarity)
 
 n_steps=30
 paper1="Worcester, MA"
