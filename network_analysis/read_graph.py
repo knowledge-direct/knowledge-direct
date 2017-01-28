e31dc5e1e3d985692afaa800547c13544b757131
siskavera <sskvera@gmail.com>
2017-01-28 17:13:02 +0000

    Added distance measure.

---
+++ network_analysis/read_graph.py
@@ -0,0 +1,18 @@
+def read_cit_HepPh():
+    import networkx as nx
+    import gzip
+    graph_file = gzip.open("../../datasets/cit-HepPh.txt.gz", "r")
+
+    citation_graph = nx.Graph()
+    for line in graph_file.readlines():
+        line = line.decode().strip()
+
+        if line.startswith("#"):  # skip comments
+            continue
+        else:
+            node1 = line.split()[0]
+            node2 = line.split()[1]
+            citation_graph.add_nodes_from([node1, node2]) # nodes are a set, duplicates are dealt with
+            citation_graph.add_edge(node1, node2)
+
+    return(citation_graph)
--- network_analysis/recommend_papers.py
+++ network_analysis/recommend_papers.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib
+import read_graph
 import networkx as nx
 
 def calculate_overlap(G, source_paper, target_paper):
@@ -10,15 +11,16 @@ def calculate_overlap(G, source_paper, target_paper):
     return(prop_overlap)
 
 def get_papers(G, source_paper, target_paper, max_step_size):
-    whole_path = nx.shortest_path(G, source_paper, target_paper, weight='weight')
+    whole_path = nx.shortest_path(G, source_paper, target_paper)
 
-    papers_in_path=[]
+    papers_in_path=[].append(source_paper)
     differences=[]
     cum_weights = 0
     for i in range(1,len(whole_path)):
         node1 = whole_path[i-1]
         node2 = whole_path[i]
-        this_weight=G[node1][node2]['weight']
+        #this_weight=G[node1][node2]['weight']
+        this_weight = 1
         differences.append(this_weight)
 
         cum_weights += this_weight
@@ -32,22 +34,11 @@ def get_papers(G, source_paper, target_paper, max_step_size):
     #print(differences)
     return(papers_in_path)
 
-from tutorial import miles_graph
-G=miles_graph()
+citation_graph=read_graph.read_cit_HepPh()
+print("Graph read")
 
-H = nx.Graph()
-for v in G:
-    H.add_node(v)
-for (u, v, d) in G.edges(data=True):
-    if d['weight'] < 300:
-        H.add_edge(u, v, weight=1.0)
+paper1="9907233"
+paper2="9804209"
 
-for (u,v,d) in H.edges(data=True):
-    d['weight'] = 1/calculate_overlap(H, u, v)
-
-city1="Sault Sainte Marie, MI"
-city2="West Palm Beach, FL"
-city3="Valley City, ND"
-print(G[city3])
-print(H[city3])
-#print(calculate_overlap(H, city1, city2, 800))
+#print(citation_graph.nodes())
+print(get_papers(citation_graph, paper1, paper2, 2))
